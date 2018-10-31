c46523e27d03516d031ad55d7822c99bb31e5ea9
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-10-31 12:17:23 +0000

    Minor amends and started gather and spread sections of slides.

---
+++ DataWrSlides/senate/senate.R
@@ -0,0 +1,28 @@
+## 2018 Senate Race; data from https://projects.fivethirtyeight.com/2018-midterm-election-forecast/senate/?ex_cid=rrpromo
+
+## load libraries
+library(tidyverse)
+
+## read in data
+senate <- read_csv("senate_seat_forecast.csv")
+
+## extract subset of data
+senate <- senate %>%
+    filter(forecastdate == max(forecastdate)) %>%
+    filter(model == "classic") %>%
+    filter(party != "false") %>%
+    select(state, party, win_probability) %>%
+    mutate(id = 1:n()) %>%
+    spread(party, win_probability) %>%
+    select(-id)
+
+## save output
+saveRDS(senate, "senate.rds")
+
+## check
+senate %>%
+    gather(party, prob, -state) %>%
+    filter(!is.na(prob)) %>%
+    group_by(state) %>%
+    summarise(sum = sum(prob)) %>%
+    arrange(-sum)

8a3da5667aa961f020f8a34faa541e9c8a389242
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-11-04 12:07:08 +0000

    Updated literate programming notes.

--- litprog/ff.R
+++ litprog/ff.R
@@ -1,25 +1,23 @@
+## load libraries
+library(tidyverse)
 
-``{r, eval = F}
-## load the data
-ff <- readRDS("ff.rds")
+## load data
+ff <- read_csv("ff.csv")
 
-## summarise data
-summary(ff)
+## convert columns to correct format
+## and simplify column names
+ff <- mutate(ff, partners = as.character(partners)) %>%
+    rename(partner_type = `partner type`)
 
-## plot thorax against group
-plot(thorax ~ group, data = ff, 
-     ylab = "Length of thorax (mm)", 
-     xlab = "Experimental group")
-     
-## plot longevity against group
-plot(longevity ~ group, data = ff,
-     ylab = "Longevity (days)", 
-     xlab = "Experimental group")
-
-## plot longevity against thorax
-plot(longevity ~ thorax, data = ff, 
-     pch = 20, 
-     xlab = "Length of thorax (mm)",
-     ylab = "Longevity (days)")
-``
-
+## produce summary plot
+ggplot(ff, aes(y = longevity, x = thorax, 
+               linetype = partner_type, 
+               shape = partner_type)) +
+    geom_point() +
+    stat_smooth(method = "lm", se = FALSE) +
+    facet_wrap(~partners) +
+    labs(linetype = "Partner Type", 
+         shape = "Partner Type") +
+    ylab("Longevity (days)") +
+    xlab("Thorax length (mm)") +
+    theme_bw()
